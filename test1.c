adb85f2e9665f398dab4fe9eb5ab63c8c6d9124c
Angelay2 <y2zaijian@126.com>
2020-03-19 23:51:41 +0800

    链表(增删查改)

--- test1.c
+++ test1.c
@@ -1,30 +1,39 @@
+#include <stdio.h>
+#include <windows.h>
 
 // 3. 链表
 // 链表:逻辑上连续 物理上不连续 不是一条线 而是一环扣一环 
 // 每一个数据单元中不光有数据 还有指针 指向下一个元素的起始位置\
-// 数据单元是结构体(单向) 
-// 8中链表结构
-// 1. 单链表不带头非循环链表 做底层结构的实现(栈的底层实现)
-// 一个数据单元包括一个数据和指针, 数据单元不是挨着的, 通过指针连接下一个数据
+// 一个数据单元包括一个数据和指针, 数据单元不是挨着的, 通过指针连接下一个数据, 都相当于是一个结构体
 // 所以是逻辑连续, 物理不一定连续的线性表
-typedef struct Node{
-	int _data;
-	struct Node* _next;
+// 数据单元是结构体(单向) 
+// 8种链表结构
+// 常用的有2种: 
+// 1. 单链表不带头非循环链表( 做底层结构的实现(栈的底层实现) ) 
+// 2. 双链表带头循环结构
+// Node为结点(一个数据单元) sList表示整个单链表(本次实现的为无头非循环单链表)
+
+typedef struct Node{// 定义一个结点
+	int _data;// 数据
+	struct Node* _next;// 指向下一个指针
 }Node;// 数据单元用结构体表示
 // 单链表不需要记录每个节点 记录一个头(第一个节点的起始位置)
 
-// 实现'/'''''''''无头非循环单链表
+// 实现无头非循环单链表(所以一开始是一个空链表)
+// (成员为头结点)
 typedef struct sList{
-	Node* _head;
+	Node* _head;// 单链表只需要记录第一个结点的起始位置
 }sList;
 
+// 单链表初始化, 放的是结构体(sList本身也需要给一个结构体)
 void sListInit(sList* sl){
 	// 空链表
 	// 新的头指向新的节点 指向newNode 不指向原来的节点 指向新的
 	// 先创建新节点
-	sl->_head = NULL;
+	sl->_head = NULL;// 让头指向NULL(无效地址)
 }
 
+// 打印
 void printsList(sList* sl){
 	Node* cur = sl->_head;
 	while (cur){
@@ -32,30 +41,105 @@ void printsList(sList* sl){
 		cur = cur->_next;
 	}
 }
-Node* createNode(int* data){
+
+//创建新的结点(进行封装)
+//没必要传指针, 新结点指向空就行了
+Node* createNode(int data){
+	// 先申请空间
 	Node* node = (Node*)malloc(sizeof(Node));
 	node->_data = data;
 	node->_next = NULL;
 	return node;
 }
+
+// 头插
+// 让单链表的头(sL->head)不在指向原来的第一个结点, 而指向newNode ,让newNoded->next指向之前的第一个结点
 void sListPushFront(sList* sl, int data){
-	// 每创建一个 next都指向NULL
+	// 每创建一个node(新结点) next都指向NULL
 	Node* node = createNode(data);
 
-	// 判断链表是否为空 (只有一个节点的链表)只有第一次进if
+	// 判断原始链表是否为空 由于链表进行了初始化, 所以第一次会进if(也只有第一次进if)
+	// 如果原始链表为空, 就让新结点node作为我们的头(变成只有一个节点的链表)
 	if (sl->_head == NULL){
 		sl->_head = node;
 	}
 	else{
 		// 头插
-		node->_next = sl->_head;// 
+		// sl->head原来指向的就是链表的第一个结点 赋给新结点的next后再指向新的结点
+		node->_next = sl->_head;
 		sl->_head = node;
 	}
 }
 
+// 头删(删除头结点)修改头指针的指向
+// 让 sl->head 不再指向原始的第一个结点 指向第一个结点的下一个结点, 然后释放原始头结点
+// 首先把原来的头保存下来,         node = sl->_head
+// 再让sl->_head 指向下一个结点    sl->head = sl->_head->_next,
+// 再释放原始头结点(保存在node里)  free(node)
 void sListPopFront(sList* sl){
+	Node* node = (Node*)malloc(sizeof(Node));// head的类型为Node* 所以用Node*定义node
+	// 空表不需要删除
+	if (sl->_head == NULL)
+		return;
+	node = sl->_head;
+	sl->_head = sl->_head->_next;
+	free(node);
 
 }
+
+/*尾插
+给定一个新结点(newNode),让最后一个结点的next指向newNode, 而newNode的next指向NULL
+首先应该找到最后一个非空的结点,
+定义一个指针cur 将链表遍历一遍, 只要cur->_next不为NULL, 就继续向后走
+当cur->next为NULL时, 在cur后面插入新结点,*/
+void sListPushBack(sList* sl, int data){
+
+	// 找到最后一个结点
+	Node* cur = sl->_head;
+	// 
+	while (cur->_next){
+		cur = cur->_next;// 向后走一步
+	}
+	// 到这里cur指向了最后一个非空结点
+	cur->_next = createNode(data);
+}
+
+// 尾删
+// 删除最后一个节点后, 要让最后一个结点的前一个结点(前驱)->next = NULL,
+// 设置3个指针: sl->head, cur(当前), prev(前驱, 一开始可以使一个NULL的位置, )
+// 当cur指向第二个时, prev指向第一个,cur往前移动一位,prev跟着往前一位
+// 如果cur的next不为NULL就一直走,当cur->next = NULL时,要删掉cur
+// 让prev->next不再指向cur,指向NULL
+// 如果链表中只有一个结点的话 只需要让sl->head = NULL 就可以了,
+void sListPopBack(sList* sl){
+	Node* cur, *prev;
+	// 空链表
+	if (sl->_head == NULL)
+		return;
+	// 不是空链表 但是只有一个头结点
+	if (sl->_head->_next == NULL){
+		// 直接把头free后就相当于删除头结点, 之后变成空链表
+		sl->_head = NULL;
+		free(sl->_head);
+	}
+	// 有多个结点
+	else{
+		prev = NULL;
+		cur = sl->_head;
+		while (cur->_next){
+			prev = cur;
+			cur = cur->_next;
+		}
+		if (prev == NULL){
+			sl->_head = NULL;
+		}
+		// cur指向最后一个结点, 让前驱指向NULL
+		prev->_next = NULL;
+		free(cur);
+	}
+}
+
+// 在单链表中找一个值
 // 应返回在当前链表的位置 所以返回值为Node*
 Node* sListFind(sList* sl, int data){
 	Node* cur = sl->_head;
@@ -66,76 +150,59 @@ Node* sListFind(sList* sl, int data){
 	}
 	return NULL;
 }
-// 要在前面插, 要遍历单链表 要先找到结点的前驱, 他的next等于当前结点, O(N)
-// 单链表一般在结点后面插入, 时间复杂度为O(1)
+
+// 任意位置后插入
+// 要在单链表前面插, 要遍历单链表,O(N)
+// 先保存前驱原始的next, 让前驱的next指向新的结点, 新的结点的next指向前驱的原始next, 
+
+// 单链表一般在结点后面插入,时间复杂度为O(1), 
+// 假设在node结点后插一个newNode, 
+// 则node->next指向newNode,让newNode->next指向原来node的下一个结点
+// (先拿到node原始插入前的next 保存起来)    next = node->_next
+// 再让node的next指向newNode                node->_next = newNode
+// 再让newNode的next指向node原始的next      newNode->_next = next
+// 若为尾插, 则node->_next为NULL
+// 给定一个任意(有效)结点(Node* node), 后面插入的数据(int data)
 void sListInsertAfter(Node* node, int data){
 	if (node != NULL){
 		Node* next = node->_next;
+		// 创建新结点
 		Node* newNode = createNode(data);
 		node->_next = newNode;
 		newNode->_next = next;
 	}
 }
 
-void sListEraseAfter(Node* node){
-	if (node != NULL){
-		Node* next = node->next;
-
-	}
-
-}
-
-
-// cur向前移动一个位置 pre也跟着往前走 cur
-// 让Pre指向空,
-void sListPopBack(sList* sl){
-	Node* cur, *pre;
-	// 空链表
-	if (sl->_head == NULL){
-		return;
-	}
-	// 只有一个头结点		
-	if (sl->_head->_next == NULL){
-		// 删除之后编程空链表
-		free(sl->_head);
-		sl->_head = NULL;
-	}
-	else{
-		pre = NULL;
-		cur = sl->_head;
-		while (cur-> - next){
-			pre = cur;
-			cur = cur->_next;
-		}
-
-		//if (pre == NULL){
-		//	// 相当于只有一个头节点
-		//	sl->_head = NULL;
-		//}
-		//else{
-		//	pre->_next = NULL;
-		//}
-		pre->_next = NULL;
-		free(cur);
-	}
+//// 任意位置删除
+//void sListEraseAfter(Node* node){
+//	if (node != NULL){
+//		Node* next = node->next;
+//
+//	}
+//
+//}
 
 
-}
-
+// 单链表销毁
 void sListDestory(sList* sl){
 
 }
 
 void testsList(){
-	sList sl;
-	sListInit(&sl);
+	sList sl;// 定义链表
+	sListInit(&sl);// 初始化(链表置空)
 	sListPushFront(&sl, 1);
 	sListPushFront(&sl, 2);
 	sListPushFront(&sl, 3);
 	sListPushFront(&sl, 4);
 	sListPushFront(&sl, 5);
-	sListPushFront(&sl, 6);
-
-	printsList(&sl);// 打印出6,5,4,3,2,1
+	sListPushFront(&sl, 6);// 尾插
+	sListPopFront(&sl);// 头删
+	sListPopBack(&sl);// 尾删
+	printsList(&sl);// 打印
 }
-// 2. 双向带头非循环 
+int main(){
+	testsList();
+	system("pause");
+	return 0;
+}
